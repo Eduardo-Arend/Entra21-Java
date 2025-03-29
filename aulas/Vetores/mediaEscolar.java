b7c4ff7bc4bea8d4ad5b58014f2b86910ba60c7d
Eduardo-Arend <educaa.az@hotmail.com>
2025-03-29 15:36:12 -0300

    Conclusão do exercicio 11

--- aulas/Vetores/mediaEscolar.java
+++ aulas/Vetores/mediaEscolar.java
@@ -1,5 +1,9 @@
 package Vetores;
 
+/*a quantidade de alunos na turma, depois a quantidade de notas depois a quantidade de notas ,
+ depois pra cada aluno ler as notas que ele teve, depois mostrar qual a média de cada um, qual menor nota de todos ,
+  qual maior nota de todos, e qual a média geral */
+
 import java.util.Scanner;
 
 public class mediaEscolar {
